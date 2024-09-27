1730c2d8ae29156aa3486cc4da74617367500d32
Therax1 <nabilmoutouama@gmail.com>
2024-09-27 14:58:44 +0100

    Première partie du formulaire maîtrisée avec succès. Statut : (Terminé)

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ let bouton = document.querySelector('.buttonaction')
 let body = document.querySelector('body')
 let part1 = document.querySelector('.part1')
 let part2 = document.querySelector('form')
+let boutonext = document.querySelector('#bouttonsuivant')
+let form1 = document.querySelector('.form-1')
+let form2 = document.querySelector('.form-2')
 
 bouton.addEventListener('click', function (){
     body.style.flexDirection = "row";
@@ -11,4 +14,9 @@ bouton.addEventListener('click', function (){
     bouton.setAttribute('hidden', 'hidden');
     part1.style.width = "50%";
     part2.style.width = "50%"
+})
+
+boutonext.addEventListener('click', function(){
+    form1.setAttribute('hidden', 'hidden');
+    form2.removeAttribute('hidden');
 })
